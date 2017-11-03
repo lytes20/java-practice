a38ef39d119dbd3a4c83ad6b5bdcbf606ef46b6c
Bamuleseyo Gideon <gideonbamuleseyo@gmail.com>
2017-11-03 10:03:20 +0300

    leetcode sum two integers in an array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,27 +2,31 @@
 
 package com.company;
 import java.lang.Character;
-
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Some of my tests
-        char [] emptyCharArray = {};
-        char [] oneElementArray = {'a'};
-        char [] charArrayEndingWithCapital = {'a', 'K', 'c', 'D', 'E'};
-        char [] myCharArray = {'a', 'K', 'c', 'D', 'E', 'l'};
+//        // Some of my tests
+//        char [] emptyCharArray = {};
+//        char [] oneElementArray = {'a'};
+//        char [] charArrayEndingWithCapital = {'a', 'K', 'c', 'D', 'E'};
+//        char [] myCharArray = {'a', 'K', 'c', 'D', 'E', 'l'};
+//
+//        /*unhandled (untested tests) tests for now
+//         * Array containing numbers
+//         * Array with all Lower Case
+//         * Array with all Upper
+//          * */
+//
+//
+//        lowerUpperCase(myCharArray);
 
-        /*unhandled (untested tests) tests for now
-         * Array containing numbers
-         * Array with all Lower Case
-         * Array with all Upper
-          * */
-
-
-        lowerUpperCase(myCharArray);
+        int [] leNums = twoSum(new int[]{3,3}, 6);
+        System.out.println(Arrays.toString(leNums));
 
     }
 
@@ -73,6 +77,36 @@ public class Main {
         char temp = input[i];
         input[i] = input[j];
         input[j] = temp;
+
+    }
+
+
+    // Add two numbers in an array
+    public static int[] twoSum(int[] nums, int target) {
+        int sum = 0;
+        int [] leNums = null;
+        for (int i = 0; i<nums.length; i++){
+            for(int j =1; j<nums.length; j++){
+
+                if(nums[i] == nums[j]) {
+                    j = j+1;
+                    if(j > nums.length){
+                        break;
+                    }
+
+                }
+
+                sum = nums[i] + nums [j];
+                if (sum == target) {
+                    return  new int[]{i, j};
+
+                }
+            }
+
+            sum = 0;
+        }
+
+        return null;
 
     }
 }
